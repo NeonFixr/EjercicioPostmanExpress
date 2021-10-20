ada6e9f46948f768ffd4adf9575c54217db7875a
Yago <86063460+NeonFixr@users.noreply.github.com>
2021-10-20 12:41:11 +0200

    usando postman y filtrando

--- index.js
+++ index.js
@@ -9,13 +9,33 @@ const PORT = 3000;
 //Middleware
 // app.use(morgan('combined'));
 app.use(morgan('combined', { stream: logger.stream }));
+app.use(express.json());
 
+const movies = [
+{id:1, title:"Soy Leyenda"},
+{id:2, title:"Aliens"},
+{id:3, title:"Blade Runner 3049"},
+{id:4, title:"Dune"},
+]
 
 app.get('/', (req, res) => {
     let date = new Date();
     //console.log(`home route works ------  ${date}`);
     res.send('Bienvenidos a Express');
 });
+
+//End-poins CRUD movies
+app.get('/movies',(req, res) => {
+    res.json(movies);
+})
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    // const {id} = req.params.id;
+    let movie = movies.find(movie => movie.id == id);
+    res.json(movie);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`.bgGreen.black);
